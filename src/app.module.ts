40cd7c791c03d71ab6af7b9a5c2e0768a2b8f0c0
UkYi <ukyi.js@gmail.com>
2020-09-15 16:20:47 +0900

    feat: pokemon module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     CacheModule.registerAsync({ useClass: CacheService }),
     GraphQLModule.forRootAsync({ useClass: GraphqlService }),
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
+    PokemonModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
