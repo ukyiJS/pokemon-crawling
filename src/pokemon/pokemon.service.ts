67e355182ad871ae4abd233d33fefd8755e692b0
UkYi <ukyi.js@gmail.com>
2020-09-16 04:23:43 +0900

    feat: getEvolutionChanByLevel 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,15 +1,15 @@
 import { getBrowserAndPage } from '@/utils';
 import { Injectable } from '@nestjs/common';
-import { EvolutionChainByLevel, IEvolutionChain } from './evolution';
+import { getEvolutionChainByLevel, IEvolutionChain } from './evolution';
 
 @Injectable()
 export class PokemonService {
-  public async getEvolutionChanByLevel(): Promise<IEvolutionChain> {
+  public async getEvolutionChanByLevel(): Promise<IEvolutionChain[]> {
     const url = 'https://pokemondb.net/evolution/level';
     const waitForSelector = '#evolution > tbody';
     const { browser, page } = await getBrowserAndPage(url, waitForSelector);
 
-    const result = await new EvolutionChainByLevel().getEvolutionByLevel(page);
+    const result = await getEvolutionChainByLevel(page);
     await browser.close();
 
     return result;
