868210dba16f0754dda83051d3f253a2bd6af4ce
UkYi <ukyi.js@gmail.com>
2020-09-15 16:02:24 +0900

    feat: app module 추가

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { CacheService, GraphqlService, TypeormService } from '@/config';
+import { CacheModule, Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({ useClass: CacheService }),
+    GraphQLModule.forRootAsync({ useClass: GraphqlService }),
+    TypeOrmModule.forRootAsync({ useClass: TypeormService }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
