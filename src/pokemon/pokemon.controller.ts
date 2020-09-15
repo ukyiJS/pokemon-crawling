8a96e801845359b7a4104e92a2c5d0a0d2f14c7a
UkYi <ukyi.js@gmail.com>
2020-09-16 04:23:29 +0900

    feat: getEvolutionChanByLevel 추가

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IEvolutionChain } from './evolution';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
-export class PokemonController {}
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Get('evolutionChainByLevel')
+  public getEvolutionChanByLevel(): Promise<IEvolutionChain[]> {
+    return this.pokemonService.getEvolutionChanByLevel();
+  }
+}
