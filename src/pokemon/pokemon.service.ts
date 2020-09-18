6c3b67ce159bc294768a91f80ef8419f8f450dae
UkYi <ukyi.js@gmail.com>
2020-09-18 22:04:12 +0900

    feat: pokemon interface 추가 및 json util 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,25 +1,50 @@
+import { writeJson } from '@/utils';
 import { Injectable } from '@nestjs/common';
-import { EvolutionType, getEvolutionChains, IEvolutionChain } from './evolutionChains';
+import { EvolutionType, getEvolutionChains } from './evolutionChains';
+import { IEvolutionChain } from './pokemon.interface';
 
 @Injectable()
 export class PokemonService {
-  public getEvolutionChainByLevel(): Promise<IEvolutionChain[]> {
-    return getEvolutionChains(EvolutionType.LEVEL);
+  public async getEvolutionChainByLevel(): Promise<IEvolutionChain[]> {
+    const evolutionChainByLevel = await getEvolutionChains(EvolutionType.LEVEL);
+    writeJson({ data: evolutionChainByLevel, fileName: 'evolutionChainByLevel', dirName: 'src/assets/json' });
+
+    return evolutionChainByLevel;
   }
 
   public async getEvolutionChainByElementalStone(): Promise<IEvolutionChain[]> {
-    return getEvolutionChains(EvolutionType.STONE);
+    const evolutionChainByElementalStone = await getEvolutionChains(EvolutionType.STONE);
+    writeJson({
+      data: evolutionChainByElementalStone,
+      fileName: 'evolutionChainByElementalStone',
+      dirName: 'src/assets/json',
+    });
+
+    return evolutionChainByElementalStone;
   }
 
   public async getEvolutionChainByTrading(): Promise<IEvolutionChain[]> {
-    return getEvolutionChains(EvolutionType.TRADE);
+    const evolutionChainByTrading = await getEvolutionChains(EvolutionType.TRADE);
+    writeJson({ data: evolutionChainByTrading, fileName: 'evolutionChainByTrading', dirName: 'src/assets/json' });
+
+    return evolutionChainByTrading;
   }
 
   public async getEvolutionChainByFriendship(): Promise<IEvolutionChain[]> {
-    return getEvolutionChains(EvolutionType.FRIENDSHIP);
+    const evolutionChainByFriendship = await getEvolutionChains(EvolutionType.FRIENDSHIP);
+    writeJson({ data: evolutionChainByFriendship, fileName: 'evolutionChainByFriendship', dirName: 'src/assets/json' });
+
+    return evolutionChainByFriendship;
   }
 
   public async getEvolutionChainByOtherCondition(): Promise<IEvolutionChain[]> {
-    return getEvolutionChains(EvolutionType.STATUS);
+    const evolutionChainByOtherCondition = await getEvolutionChains(EvolutionType.STATUS);
+    writeJson({
+      data: evolutionChainByOtherCondition,
+      fileName: 'evolutionChainByOtherCondition',
+      dirName: 'src/assets/json',
+    });
+
+    return evolutionChainByOtherCondition;
   }
 }
