7b6eed268b4c3023d3e8951fce979f1e5a4278a1
UkYi <ukyi.js@gmail.com>
2020-09-18 22:02:14 +0900

    feat: json util 추가

---
+++ src/utils/json.ts
@@ -0,0 +1,45 @@
+import { Logger } from '@nestjs/common';
+import { existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+interface WriteJson {
+  data: any;
+  fileName: string;
+  dirName?: string;
+}
+
+interface GetJson {
+  dirName?: string;
+  fileName: string;
+}
+
+interface MergedJson {
+  fileNames: string[];
+  dirName?: string;
+}
+
+const fileOptions = { encoding: 'utf-8' };
+
+export const getJson = <T>({ fileName, dirName = 'src/assets/json' }: GetJson): T => {
+  const dir = join(process.cwd(), dirName, fileName);
+  return JSON.parse(readFileSync(dir, fileOptions)) as T;
+};
+
+export const mergeJson = <T>({ fileNames, dirName = 'src/assets/json' }: MergedJson): T[] => {
+  const dir = join(process.cwd(), dirName);
+  const jsonNames = readdirSync(dir).filter(name => fileNames.includes(name));
+  return jsonNames.map<T>(json => JSON.parse(readFileSync(join(dir, json), fileOptions)));
+};
+
+export const writeJson = ({ data, fileName, dirName = '' }: WriteJson): void => {
+  const saveDir = join(process.cwd(), dirName);
+  const jsonFileName = `${saveDir}/${fileName}.json`;
+
+  if (!existsSync(saveDir)) {
+    mkdirSync(saveDir);
+    Logger.log(saveDir, 'CreateDirectory');
+  }
+
+  writeFileSync(jsonFileName, JSON.stringify(data));
+  Logger.log(jsonFileName, 'WriteFile');
+};
