0115836daf4625bd74f9afb1ba384c1753300a12
UkYi <ukyi.js@gmail.com>
2020-09-23 18:25:13 +0900

    feat: getPokedex 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,8 @@
-import { mergeJson } from '@/utils';
+import { getBrowserAndPage, mergeJson } from '@/utils';
 import { Injectable } from '@nestjs/common';
 import { readdirSync } from 'fs';
 import { join } from 'path';
+import { IPokedex } from './common/type';
 import { EvolutionType, getEvolutionChains } from './evolutionChains';
 import { IEvolutionChain } from './pokemon.interface';
 
@@ -61,5 +62,43 @@ export class PokemonService {
         if (a.name > b.name) return 1;
         return 0;
       });
+  }
+
+  public async getPokedex(): Promise<IPokedex[]> {
+    const url = 'https://pokemondb.net/pokedex/all';
+    const waitForSelector = '#pokedex > tbody';
+    const { browser, page } = await getBrowserAndPage(url, waitForSelector);
+
+    const crawlingData = await page.$$eval('#pokedex > tbody > tr', el => {
+      return Array.from(el).reduce<IPokedex[]>((acc, $tr) => {
+        const no = $tr.querySelector('.infocard-cell-data')!.textContent!;
+        const $image = $tr.querySelector('.icon-pkmn')!;
+        const image = $image.getAttribute('data-src') ?? ($image as HTMLImageElement).src;
+        const name = $tr.querySelector('.ent-name')!.textContent;
+        const form = $tr.querySelector('.text-muted')?.textContent ?? null;
+        const index = acc.findIndex(pokemon => pokemon.no === no);
+        const isOverlapPokemon = index > -1 && !form;
+
+        if (/partner/i.test(form ?? '') || isOverlapPokemon) return acc;
+
+        const types = Array.from($tr.querySelectorAll('.cell-icon a')).map(a => a.textContent);
+        const totalStat = { name: '총합', value: $tr.querySelector('.cell-total')!.textContent };
+        const statNames = ['HP', '공격', '방어', '특수공격', '특수방어', '스피드'];
+        const stats = Array.from($tr.querySelectorAll('.cell-num:not(.cell-fixed)')).map((stat, i) => ({
+          name: statNames[i],
+          value: +stat.textContent!,
+        }));
+        const result = { no, name, image, form, types, stats: [...stats, totalStat] };
+
+        if (index > -1) {
+          acc[index].differentForm = [...acc[index].differentForm, result] as IPokedex[];
+          return acc;
+        }
+
+        return [...acc, { ...result, differentForm: [] }] as IPokedex[];
+      }, []);
+    });
+    await browser.close();
+    return crawlingData;
   }
 }
