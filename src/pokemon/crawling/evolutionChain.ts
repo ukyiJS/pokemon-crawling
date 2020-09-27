c64f059200f6d3dd78f2282e6cb741f939f7d5c8
UkYi <ukyi.js@gmail.com>
2020-09-27 23:01:02 +0900

    fix: this 바인딩 문제 수정 및 initCrawlingUtils 분리

--- src/pokemon/crawling/evolutionChain.ts
+++ src/pokemon/crawling/evolutionChain.ts
@@ -1,64 +1,18 @@
-import { Page } from 'puppeteer';
 import { EvolutionType } from '../pokemon.enum';
-import { IEvolutionChain, IEvolvingTo, IWindow } from '../pokemon.interface';
+import { IEvolutionChain, IWindow } from '../pokemon.interface';
 import { PokemonForm } from './pokemonForm';
 
 declare let window: IWindow;
 
 export class EvolutionChain {
-  page: Page;
-
   evolutionType: EvolutionType;
 
-  constructor(page: Page, evolutionType: EvolutionType) {
-    this.page = page;
+  constructor(evolutionType: EvolutionType) {
     this.evolutionType = evolutionType;
-
-    this.initCrawlingUtils();
   }
 
-  private async initCrawlingUtils(): Promise<void> {
-    await this.page.evaluate(() => {
-      window.getPokemonInfo = ($element: Element): IEvolutionChain => {
-        const $image = $element.querySelector('.icon-pkmn')!;
-
-        const name = $element.querySelector('.ent-name')!.textContent!;
-        const image = $image.getAttribute('data-src') ?? ($image as HTMLImageElement).src;
-        const form = $element.querySelector('.text-muted')?.textContent ?? null;
-
-        return { name, image, form, differentForm: [], evolvingTo: [] };
-      };
-
-      window.getEvolvingTo = ($element: Element, to: IEvolutionChain, type: string): IEvolvingTo => ({
-        ...to,
-        type,
-        level: $element.querySelector('.cell-num')?.textContent ?? null,
-        condition: $element.querySelector('.cell-med-text')?.textContent || null,
-      });
-
-      window.addFromEvolvingTo = (acc: IEvolutionChain[], index: number, chain: IEvolutionChain): IEvolutionChain[] => {
-        acc[index].evolvingTo = [chain] as IEvolvingTo[];
-        return acc;
-      };
-
-      window.addMultipleEvolvingTo = (acc: IEvolutionChain[], index: number, to: IEvolvingTo): IEvolutionChain[] => {
-        acc[index].evolvingTo = [...acc[index].evolvingTo, to];
-        return acc;
-      };
-
-      window.addFromDifferentForm = (
-        acc: IEvolutionChain[],
-        index: number,
-        chain: IEvolutionChain,
-      ): IEvolutionChain[] => {
-        acc[index].differentForm = [...acc[index].differentForm, chain] as IEvolutionChain[];
-        return acc;
-      };
-    });
-  }
-
-  public crawling($elements: Element[], type: string): IEvolutionChain[] {
-    return $elements.reduce<IEvolutionChain[]>((acc, $tr) => {
+  public crawling = ($elements: Element[], type: string): IEvolutionChain[] =>
+    $elements.reduce<IEvolutionChain[]>((acc, $tr) => {
       const $cellNames = Array.from($tr.querySelectorAll('.cell-name'));
       const [from, to] = $cellNames.map(window.getPokemonInfo);
       const evolvingTo = window.getEvolvingTo($tr, to, type);
@@ -75,10 +29,9 @@ export class EvolutionChain {
 
       return [...acc, chain] as IEvolutionChain[];
     }, []);
-  }
 
-  public convertIntoKor(evolutionChains: IEvolutionChain[]): IEvolutionChain[] {
+  public convertIntoKor = (evolutionChains: IEvolutionChain[]): IEvolutionChain[] => {
     const { convertFormIntoKor } = new PokemonForm(this.evolutionType);
     return evolutionChains.map(convertFormIntoKor);
-  }
+  };
 }
