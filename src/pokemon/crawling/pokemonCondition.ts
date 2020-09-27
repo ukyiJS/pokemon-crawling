278a0b4d32ae76a1e8110a1d6c89f6ffb494c9cc
UkYi <ukyi.js@gmail.com>
2020-09-27 23:00:33 +0900

    fix: this 바인딩 문제 수정

--- src/pokemon/crawling/pokemonCondition.ts
+++ src/pokemon/crawling/pokemonCondition.ts
@@ -10,7 +10,7 @@ export class PokemonCondition {
     this.evolutionType = evolutionType;
   }
 
-  private getConditionEnum(type: EvolutionType): Condition {
+  private getConditionEnum = (type: EvolutionType): Condition => {
     switch (type) {
       case EvolutionType.LEVEL:
         return LevelCondition;
@@ -19,17 +19,17 @@ export class PokemonCondition {
       default:
         return TradingCondition;
     }
-  }
+  };
 
-  private getCondition(condition: string | null, ConditionEnum: Condition): string | null {
+  private getCondition = (condition: string | null, ConditionEnum: Condition): string | null => {
     if (!condition) return null;
 
     const hasForm = (regExp: string | RegExp): boolean => new RegExp(regExp, 'i').test(condition);
     const key = Object.keys(ConditionEnum).find(key => hasForm(key.replace(/_/, '')));
     return key ? ConditionEnum[key as keyof typeof ConditionEnum] : null;
-  }
+  };
 
-  private convertCondition(conditions: string | null, type: EvolutionType): string | null {
+  private convertCondition = (conditions: string | null, type: EvolutionType): string | null => {
     if (!conditions) return null;
 
     const [c1, c2] = conditions.split(',');
@@ -37,17 +37,13 @@ export class PokemonCondition {
 
     const convertedCondition = this.getCondition(c1, this.getConditionEnum(type));
     return additionalCondition ? `${convertedCondition}, ${additionalCondition}` : convertedCondition;
-  }
+  };
 
-  private deepConvertCondition(to: IEvolvingTo): IEvolvingTo {
-    return {
-      ...to,
-      condition: this.convertCondition(to.condition, this.evolutionType),
-      evolvingTo: to.evolvingTo.map(this.deepConvertCondition),
-    };
-  }
+  private deepConvertCondition = (to: IEvolvingTo): IEvolvingTo => ({
+    ...to,
+    condition: this.convertCondition(to.condition, this.evolutionType),
+    evolvingTo: to.evolvingTo.map(this.deepConvertCondition),
+  });
 
-  public convertConditionIntoKor(to: IEvolvingTo): IEvolvingTo {
-    return this.deepConvertCondition(to);
-  }
+  public convertConditionIntoKor = (to: IEvolvingTo): IEvolvingTo => this.deepConvertCondition(to);
 }
