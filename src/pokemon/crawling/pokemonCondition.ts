1d62fde6d059c8077ba653d7f7a9c69c50b3c3a0
UkYi <ukyi.js@gmail.com>
2020-09-28 15:48:24 +0900

    fat 생성자 제거 및 convertConditionIntoKor에서 evolutionType 바인딩 추가

--- src/pokemon/crawling/pokemonCondition.ts
+++ src/pokemon/crawling/pokemonCondition.ts
@@ -5,15 +5,12 @@ import {
   EVOLUTION_TYPE,
   FRIENDSHIP,
   LEVEL_CONDITION,
+  OTHER_CONDITION,
   TRADING_CONDITION,
 } from '../pokemon.type';
 
 export class PokemonCondition {
-  evolutionType: EVOLUTION_TYPE;
-
-  constructor(evolutionType: EVOLUTION_TYPE) {
-    this.evolutionType = evolutionType;
-  }
+  evolutionType?: EVOLUTION_TYPE;
 
   private getCondition = (condition: string | null, type?: EVOLUTION_TYPE): string | null => {
     if (!condition) return null;
@@ -33,33 +30,39 @@ export class PokemonCondition {
         case EVOLUTION_TYPE.TRADE:
           return TRADING_CONDITION;
         default:
-          return LEVEL_CONDITION;
+          return OTHER_CONDITION;
       }
     })();
 
-    const key = Object.keys(CONDITION).find(key => hasForm(key.replace(/_/, '')));
+    const key = Object.keys(CONDITION).find(key => hasForm(key.replace(/_/g, ' ')));
     const convertedCondition = CONDITION[key as keyof typeof CONDITION] ?? null;
 
     return convertedCondition;
   };
 
-  private convertCondition = (conditions: string | null, type: EVOLUTION_TYPE): string | null => {
-    if (!conditions) return null;
+  private convertCondition = (conditions: string | null): string | null => {
+    if (!conditions || this.evolutionType) return null;
 
     const [c1, c2] = conditions.split(',');
     const additionalCondition = this.getCondition(c2)?.concat(', ') ?? '';
-    const convertedCondition = this.getCondition(c1, type);
+    const convertedCondition = this.getCondition(c1, this.evolutionType);
 
-    if (type === EVOLUTION_TYPE.FRIENDSHIP) return `${convertedCondition?.concat(' ') ?? ''}${FRIENDSHIP.NONE} 레벨업`;
+    if (this.evolutionType === EVOLUTION_TYPE.FRIENDSHIP)
+      return `${convertedCondition?.concat(' ') ?? ''}${FRIENDSHIP.NONE} 레벨업`;
 
     return additionalCondition ? `${additionalCondition}${convertedCondition}` : convertedCondition;
   };
 
-  private deepConvertCondition = (to: IEvolvingTo): IEvolvingTo => ({
-    ...to,
-    condition: this.convertCondition(to.condition, this.evolutionType),
-    evolvingTo: to.evolvingTo.map(this.deepConvertCondition),
-  });
+  private deepConvertCondition = (to: IEvolvingTo): IEvolvingTo => {
+    return {
+      ...to,
+      condition: this.convertCondition(to.condition),
+      evolvingTo: to.evolvingTo.map(this.deepConvertCondition),
+    };
+  };
 
-  public convertConditionIntoKor = (to: IEvolvingTo): IEvolvingTo => this.deepConvertCondition(to);
+  public convertConditionIntoKor = (to: IEvolvingTo, evolutionType: EVOLUTION_TYPE): IEvolvingTo => {
+    this.evolutionType = evolutionType;
+    return this.deepConvertCondition(to);
+  };
 }
