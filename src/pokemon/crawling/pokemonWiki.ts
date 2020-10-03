cd23bf4f05f0754d4c9774056df19e30e75f1598
UkYi <ukyi.js@gmail.com>
2020-10-04 01:56:39 +0900

    feat: crawling 추가

---
+++ src/pokemon/crawling/pokemonWiki.ts
@@ -0,0 +1,38 @@
+import { Logger } from '@nestjs/common';
+import { Page } from 'puppeteer';
+import { IPokemonWiki } from '../pokemon.interface';
+
+export class PokemonWiki {
+  private loopCount: number;
+
+  constructor(loopCount = 892) {
+    this.loopCount = loopCount;
+  }
+
+  public crawling = async (page: Page): Promise<IPokemonWiki[]> => {
+    let currentCount = 0;
+    let pokemons: IPokemonWiki[] = [];
+
+    const nextClickSelector = 'table.w-100.mb-1 td:nth-child(3) td:nth-child(1) > a';
+    const navigationPromise = page.waitForNavigation();
+
+    do {
+      const $infobox = await page.waitForSelector('.infobox-pokemon');
+      const pokemon = await page.evaluate(this.getPokemons, $infobox);
+      pokemons = [...pokemons, pokemon];
+
+      currentCount = +pokemon.no;
+      Logger.warn(this.loopCount, 'loopCount');
+
+      const percent = `${Math.floor((currentCount / this.loopCount) * 100)}%`;
+      Logger.debug(`${JSON.stringify(pokemon)}`, 'Result');
+      Logger.verbose(`############################## ${percent} ##############################`, 'Result');
+
+      await page.waitForSelector(nextClickSelector);
+      await page.click(nextClickSelector);
+      await navigationPromise;
+    } while (currentCount < this.loopCount);
+
+    return pokemons;
+  };
+}
