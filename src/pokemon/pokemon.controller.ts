dbb0dcea1243aeea777e913a7c4ad4149b13725f
UkYi <ukyi.js@gmail.com>
2020-10-21 12:38:08 +0900

    feat: getEvolutionOfDatabase 추가

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,42 +1,12 @@
 import { WriteJsonInterceptor } from '@/common';
 import { Controller, Get, UseInterceptors } from '@nestjs/common';
-import { IPokemon, IPokemonsOfDatabase, IPokemonsOfWiki } from './pokemon.interface';
+import { IEvolution, IPokemonsOfDatabase, IPokemonsOfWiki } from './pokemon.interface';
 import { PokemonService } from './pokemon.service';
 
 @UseInterceptors(WriteJsonInterceptor)
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
-
-  @Get('evolutionChainByLevel')
-  public getEvolutionChainByLevel(): Promise<IPokemon[]> {
-    return this.pokemonService.getEvolutionChainByLevel();
-  }
-
-  @Get('evolutionChainByElementalStone')
-  public getEvolutionChainByElementalStone(): Promise<IPokemon[]> {
-    return this.pokemonService.getEvolutionChainByElementalStone();
-  }
-
-  @Get('evolutionChainByTrading')
-  public getEvolutionChainByTrading(): Promise<IPokemon[]> {
-    return this.pokemonService.getEvolutionChainByTrading();
-  }
-
-  @Get('evolutionChainByFriendship')
-  public getEvolutionChainByFriendship(): Promise<IPokemon[]> {
-    return this.pokemonService.getEvolutionChainByFriendship();
-  }
-
-  @Get('evolutionChainByOtherCondition')
-  public getEvolutionChainByOtherCondition(): Promise<IPokemon[]> {
-    return this.pokemonService.getEvolutionChainByOtherCondition();
-  }
-
-  @Get('pokedex')
-  public getPokedex(): Promise<IPokemon[]> {
-    return this.pokemonService.getPokedex();
-  }
 
   @Get('pokemonsOfWiki')
   public getPokemonWiki(): Promise<IPokemonsOfWiki[]> {
@@ -48,13 +18,8 @@ export class PokemonController {
     return this.pokemonService.getPokemonsOfDatabase();
   }
 
-  @Get('mergedEvolutionChains')
-  public mergeEvolutionChains(): IPokemon[] {
-    return this.pokemonService.mergeEvolutionChains();
-  }
-
-  @Get('mergedPokedexAndEvolutionChains')
-  public mergePokedexAndEvolutionChains(): IPokemon[] {
-    return this.pokemonService.mergePokedexAndEvolutionChains();
+  @Get('evolutionOfDatabase')
+  public getEvolutionOfDatabase(): Promise<IEvolution[]> {
+    return this.pokemonService.getEvolutionOfDatabase();
   }
 }
