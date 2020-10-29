c1ee03e9e94faf4b0f8ef4c1e4086f41555ad70e
UkYi <ukyi.js@gmail.com>
2020-10-29 18:17:37 +0900

    feat: GALARICA_WREATH 추가

--- src/pokemon/pokemon.type.ts
+++ src/pokemon/pokemon.type.ts
@@ -160,12 +160,13 @@ export const ELEMENTAL_STONE_CONDITION = {
   SUN_STONE: '태양의돌',
   WATER_STONE: '물의돌',
   GALARICA_CUFF: '가라두구팔찌',
+  GALARICA_WREATH: '가라두구머리장식',
   SHINY_STONE: '빛의돌',
   DUSK_STONE: '어둠의돌',
   DAWN_STONE: '각성의돌',
   TART_APPLE: '새콤한 사과',
   SWEET_APPLE: '달콤한 사과',
-  CRACKED_POT: '깨진 포트 or 이빠진 포트 사용',
+  CRACKED_POT: '깨진 포트 or 이빠진 포트',
 } as const;
 export type ELEMENTAL_STONE_CONDITION = typeof ELEMENTAL_STONE_CONDITION[keyof typeof ELEMENTAL_STONE_CONDITION];
 
