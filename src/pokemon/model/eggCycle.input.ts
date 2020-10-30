e2a1c3315bb5f888978fcc061ebe10030116a355
UkYi <ukyi.js@gmail.com>
2020-10-30 22:50:28 +0900

    feat: eggCycle input 추가

---
+++ src/pokemon/model/eggCycle.input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IEggCycle } from '../pokemon.interface';
+
+@InputType()
+export class EggCycleInput implements IEggCycle {
+  @Field(() => Int)
+  cycle: number;
+
+  @Field({ nullable: true })
+  step: string | null;
+}
