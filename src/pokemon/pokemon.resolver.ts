03f2cc0309c20034f55b463ffa6055c258c5f80d
UkYi <ukyi.js@gmail.com>
2020-10-30 23:37:47 +0900

    feat: findPokemonOfDatabases 추가

--- src/pokemon/pokemon.resolver.ts
+++ src/pokemon/pokemon.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { PokemonService } from './pokemon.service';
 
 @Resolver()
-export class PokemonResolver {}
+export class PokemonResolver {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Query(() => [PokemonOfDatabase])
+  public async findPokemonOfDatabases(): Promise<PokemonOfDatabase[]> {
+    return this.pokemonService.findPokemonOfDatabases();
+  }
+}
