59898b5126033503ce6c3a7a794a25db4ed913af
UkYi <ukyi.js@gmail.com>
2020-11-03 21:03:06 +0900

    feat: DownloadImage 클래스 추가 download, multiple downloads 메서드 추가

--- src/utils/downloadImage.ts
+++ src/utils/downloadImage.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import axios, { AxiosResponse } from 'axios';
 import { createWriteStream, existsSync, mkdirSync, ReadStream } from 'fs';
 import { join } from 'path';
+import { ProgressBar } from '.';
 
 type Image = {
   url: string;
@@ -9,22 +10,41 @@ type Image = {
   dirName?: string;
 };
 
-export const downloadImage = async ({ url, name, dirName = 'download' }: Image): Promise<ReadStream> => {
-  const saveDir = join(process.cwd(), dirName);
-
-  if (!existsSync(saveDir)) {
-    mkdirSync(saveDir);
-    Logger.log(saveDir, 'CreateDirectory');
-  }
-
-  const { data }: AxiosResponse<ReadStream> = await axios({ url, responseType: 'stream' });
-
-  return new Promise((resolve, reject) => {
-    data
-      .pipe(createWriteStream(`${saveDir}/${name}`))
-      .on('finish', resolve)
-      .on('error', reject);
-
-    Logger.log(name, 'Download');
-  });
+type DataToDownload = {
+  url: string;
+  fileName: string;
 };
+
+export class DownloadImage {
+  constructor(readonly dir = join(process.cwd(), 'download')) {}
+
+  public download = async (url: string, fileName: string): Promise<ReadStream> => {
+    if (!existsSync(this.dir)) {
+      mkdirSync(this.dir);
+      Logger.log(this.dir, 'CreateDirectory');
+    }
+
+    const { data }: AxiosResponse<ReadStream> = await axios({ url, responseType: 'stream' });
+
+    return new Promise((resolve, reject) => {
+      data
+        .pipe(createWriteStream(`${this.dir}/${fileName}`))
+        .on('finish', resolve)
+        .on('error', reject);
+    });
+  };
+
+  public multipleDownloads = async (dataToDownloads: DataToDownload[]): Promise<ReadStream[]> => {
+    let result = <ReadStream[]>[];
+    const loading = new ProgressBar();
+    const loadingSize = dataToDownloads.length;
+
+    for (const [index, { url, fileName }] of dataToDownloads.entries()) {
+      result = [...result, await this.download(url, fileName)];
+      const cursor = index + 1;
+      loading.update((cursor / loadingSize) * 100, fileName);
+    }
+
+    return result;
+  };
+}
