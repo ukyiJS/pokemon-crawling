8d26210ef620939774e5e74305ad54e631c52d9e
UkYi <ukyi.js@gmail.com>
2020-11-03 17:14:13 +0900

    test: Pokemon Icon Images Test 추가

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,25 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TypeormService } from '@/config';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
 import { PokemonService } from './pokemon.service';
 
 jest.setTimeout(100000);
 describe('PokemonService', () => {
   let service: PokemonService;
+  let repository: MongoRepository<PokemonOfDatabase>;
 
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PokemonService],
+    await Test.createTestingModule({
+      imports: [TypeOrmModule.forRootAsync({ useClass: TypeormService })],
+      providers: [PokemonService, { provide: getRepositoryToken(PokemonOfDatabase), useClass: MongoRepository }],
     }).compile();
 
-    service = module.get<PokemonService>(PokemonService);
+    repository = getMongoRepository(PokemonOfDatabase);
+    service = new PokemonService(repository);
   });
 
-  it.only('Pokemons of Database Test', async () => {
+  it.skip('Pokemons of Database Test', async () => {
     const pokemons = await service.getPokemonsOfDatabase();
     expect(pokemons).not.toBeUndefined();
   });
 
-  it('Evolution Test', async () => {
+  it.skip('Evolution Test', async () => {
     const evolutions = await service.getEvolutionOfDatabase();
     expect(evolutions).not.toBeUndefined();
+  });
+
+  it.only('Pokemon Icon Images Test', async () => {
+    const iconImages = await service.getPokemonIconImagesOfSerebiiNet();
+    expect(iconImages).not.toBeUndefined();
   });
 });
