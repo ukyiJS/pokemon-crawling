fc3117f6160562fd90d9eb83f30cbccb14c02079
UkYi <ukyi.js@gmail.com>
2020-11-06 02:27:09 +0900

    feat: pokemonTypeName 추가

---
+++ src/pokemon/type/pokemonTypeName.ts
@@ -0,0 +1,21 @@
+export const pokemonTypeName = {
+  NORMAL: '노말',
+  FIRE: '불',
+  WATER: '물',
+  ELECTRIC: '전기',
+  GRASS: '풀',
+  ICE: '얼음',
+  FIGHTING: '비행',
+  POISON: '독',
+  GROUND: '땅',
+  FLYING: '비행',
+  PSYCHIC: '에스퍼',
+  BUG: '벌레',
+  ROCK: '바위',
+  GHOST: '고스트',
+  DRAGON: '드래곤',
+  DARK: '악',
+  STEEL: '강철',
+  FAIRY: '페어리',
+} as const;
+export type PokemonTypeName = typeof pokemonTypeName[keyof typeof pokemonTypeName];
