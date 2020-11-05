b01911b9d11c37f59ffc0b487daa3ad8d73935fb
UkYi <ukyi.js@gmail.com>
2020-11-06 02:28:17 +0900

    feat: getEvolutionOfDatabase 제거

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,13 +1,12 @@
-import { DownloadImage, getBrowserAndPage, getJson } from '@/utils';
+import { DownloadImage, getJson, PuppeteerUtil } from '@/utils';
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
-import { Evolution, PokemonsOfDatabase, PokemonsOfWiki } from './crawling';
+import { PokemonsOfDatabase } from './crawling/pokemonDatabase/pokemons';
+import { PokemonsOfWiki } from './crawling/pokemonWiki/pokemons';
 import { PokemonIconImages } from './crawling/serebiiNet/pokemonIconImages';
-import { CrawlingUtil } from './crawling/utils';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
-import { IEvolution, IPokemonImage, IPokemonOfDatabase, IPokemonsOfWiki } from './pokemon.interface';
-import { evolutionType, EvolutionType } from './pokemon.type';
+import { IPokemonImage, IPokemonOfDatabase, IPokemonsOfWiki } from './pokemon.interface';
 
 @Injectable()
 export class PokemonService {
@@ -19,7 +18,8 @@ export class PokemonService {
   public async getPokemonsOfWiki(): Promise<IPokemonsOfWiki[]> {
     const url = 'https://pokemon.fandom.com/ko/wiki/이상해씨';
     const selector = '.infobox-pokemon';
-    const { browser, page } = await getBrowserAndPage(url, selector);
+    const { getBrowserAndPage } = new PuppeteerUtil();
+    const { page, browser } = await getBrowserAndPage(url, selector);
     const { crawling } = new PokemonsOfWiki(page);
 
     const pokemons = await crawling();
@@ -31,6 +31,7 @@ export class PokemonService {
   public getPokemonsOfDatabase = async (): Promise<IPokemonOfDatabase[]> => {
     const url = 'https://pokemondb.net/pokedex/bulbasaur';
     const selector = '#main';
+    const { getBrowserAndPage } = new PuppeteerUtil();
     const { browser, page } = await getBrowserAndPage(url, selector);
     const { crawling } = new PokemonsOfDatabase(page);
 
@@ -38,38 +39,6 @@ export class PokemonService {
     await browser.close();
 
     return pokemons;
-  };
-
-  public getEvolutionOfDatabase = async (): Promise<IEvolution[]> => {
-    const url = (type: EvolutionType) => `https://pokemondb.net/evolution/${type}`;
-    const selector = '#evolution';
-    const { addTwiceEvolution, addMoreThanTwoKindsEvolution, addDifferentForm } = new CrawlingUtil();
-
-    let evolutions: IEvolution[] = [];
-    for (const type of Object.values(evolutionType)) {
-      if (type === evolutionType.NONE) continue;
-
-      const { browser, page } = await getBrowserAndPage(url(type), selector);
-      const [match] = page.url().match(/(?<=\/)\w+$/) ?? [];
-      if (!match) return [];
-
-      const { crawling } = new Evolution(page, match as EvolutionType);
-      const pokemons = await crawling();
-
-      evolutions = [...evolutions, ...pokemons];
-
-      await browser.close();
-    }
-
-    return evolutions
-      .reduce<IEvolution[]>((acc, evolution) => {
-        if (addTwiceEvolution(acc, evolution)) return acc;
-        if (addMoreThanTwoKindsEvolution(acc, evolution)) return acc;
-        if (addDifferentForm(acc, evolution)) return acc;
-
-        return [...acc, evolution];
-      }, [])
-      .sort((a, b) => +a.no - +b.no);
   };
 
   public async findPokemonOfDatabases(page = 1, display = 10): Promise<PokemonOfDatabase[]> {
@@ -99,6 +68,7 @@ export class PokemonService {
     if (!pokemonIconImages) {
       const url = 'https://serebii.net/pokemon/nationalpokedex.shtml';
       const selector = '#content > main';
+      const { getBrowserAndPage } = new PuppeteerUtil();
       const { browser, page } = await getBrowserAndPage(url, selector);
       const { crawling } = new PokemonIconImages(page);
 
