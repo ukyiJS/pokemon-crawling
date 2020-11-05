5f684ae773b076ace953fb14334516d49d0aca88
UkYi <ukyi.js@gmail.com>
2020-11-06 02:20:58 +0900

    feat: hangul 제거

--- src/utils/hangul.ts
+++
@@ -1,6 +0,0 @@
-import { disassemble } from 'hangul-js';
-
-export const extractJAMO = (hangul: string): string => disassemble(hangul).join('');
-
-const breakCHO = (hangul: string): string => disassemble(hangul)[0];
-export const extractCHO = (hangul: string): string => [...hangul].map(breakCHO).join('');
