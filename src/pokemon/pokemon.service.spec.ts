00878d39708735170b0c9bc9188d5041bd5d5480
UkYi <ukyi.js@gmail.com>
2020-11-06 02:29:04 +0900

    test Evolution Test 제거

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -20,17 +20,12 @@ describe('PokemonService', () => {
     service = new PokemonService(repository);
   });
 
-  it.skip('Pokemons of Database Test', async () => {
+  it.only('Pokemons of Database Test', async () => {
     const pokemons = await service.getPokemonsOfDatabase();
     expect(pokemons).not.toBeUndefined();
   });
 
-  it.skip('Evolution Test', async () => {
-    const evolutions = await service.getEvolutionOfDatabase();
-    expect(evolutions).not.toBeUndefined();
-  });
-
-  it.only('Pokemon Icon Images Test', async () => {
+  it('Pokemon Icon Images Test', async () => {
     const iconImages = await service.getPokemonIconImagesOfSerebiiNet();
     expect(iconImages).not.toBeUndefined();
   });
