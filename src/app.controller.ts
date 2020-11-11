e904e0b1d497f9e5655fa50f0c91c7ff22b32c42
UkYi <ukyi.js@gmail.com>
2020-11-11 10:38:06 +0900

    feat: getHello 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,10 @@
-import { Controller, Get, Render, Logger } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Logger, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   @Render('index.html')
-  RenderIndexPage(): void {
+  public index(): void {
     Logger.log('Render index.html', 'View');
-  }
-
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
   }
 }
