8ed1e8b093b1a0d8492de7885a9c5ced4d357d55
UkYi <ukyi.js@gmail.com>
2020-11-11 10:38:17 +0900

    feat: providers 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppResolver } from './app.resolver';
-import { AppService } from './app.service';
-import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
@@ -14,7 +11,6 @@ import { PokemonModule } from './pokemon/pokemon.module';
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
     PokemonModule,
   ],
-  controllers: [AppController, PokemonController],
-  providers: [AppService, AppResolver],
+  controllers: [AppController],
 })
 export class AppModule {}
