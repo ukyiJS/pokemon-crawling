a0cde3c03d86fe3d6fd33cf48409350dbca9dc58
UkYi <ukyi.js@gmail.com>
2020-11-11 10:35:54 +0900

    feat: cache service 제거

--- src/config/cache/index.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, CacheOptionsFactory, CacheModuleOptions } from '@nestjs/common';
-
-@Injectable()
-export class CacheService implements CacheOptionsFactory {
-  createCacheOptions(): CacheModuleOptions {
-    return { ttl: 5, max: 10 };
-  }
-}
