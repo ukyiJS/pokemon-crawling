bb054b41f8d5a84b77001109cef81cf1bc2e60a1
UkYiJS <ukyi.js@gmail.com>
2020-11-20 13:49:03 +0900

    feat: window-size 추가

--- src/utils/puppeteer.ts
+++ src/utils/puppeteer.ts
@@ -6,14 +6,19 @@ export interface BrowserAndPage {
   page: Page;
 }
 
+const width = 1920;
+const height = 1080;
+
 export class PuppeteerUtil {
   public getBrowserAndPage = async (url: string, waitForSelector: string): Promise<BrowserAndPage> => {
     const browser = await launch({
       executablePath: '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe',
+      userDataDir: '/mnt/c/Users/ukyi/AppData/Local/Google/Chrome/User Data',
       headless: false,
       devtools: true,
-      defaultViewport: { width: 1920, height: 1080 },
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: { width, height },
+      timeout: 0,
+      args: [`--window-size${width},${height}`],
       // args: [
       //   '--disable-gpu',
       //   '--disable-dev-shm-usage',
@@ -28,7 +33,7 @@ export class PuppeteerUtil {
     this.addPageEvent(page);
 
     Logger.log(`############################## Crawling Start ##############################`, 'Start');
-    await page.goto(url, { waitUntil: 'load', timeout: 0 });
+    await page.goto(url, { waitUntil: 'load' });
 
     await page.waitForSelector(waitForSelector);
 
@@ -39,7 +44,7 @@ export class PuppeteerUtil {
     page.once('domcontentloaded', () => Logger.log('✅ DOM is ready', 'DomcontentLoad'));
     page.once('load', () => Logger.log(`✅ Page is Loaded`, 'PageLoad'));
     page.on('console', message => {
-      const regExp = /^A parser-blocking|^Failed|^Access to|^No targeting|^SG Installed|^guaTrackEvent|^Created ad|^Powered by|^tracker|^Aff Overrides|^Use of|^\[AdEngine\]|^isUapResolved|^MediaWiki|^JQMIGRATE|^Watch Shows|^This page|^\[amp-analytics\/transport\]|^\[5a0391aab23c0a5\]|^\[Report Only\]|^fun-hooks|^%cAd URL:%c|^bkTags|^loaded|^\[amp-story-auto-ads:ui\]/gi;
+      const regExp = /^A parser-blocking|^Failed|^Access to|^No targeting|^SG Installed|^guaTrackEvent|^Created ad|^Powered by|^tracker|^Aff Overrides|^Use of|^\[AdEngine\]|^isUapResolved|^MediaWiki|^JQMIGRATE|^Watch Shows|^This page|^\[amp-analytics\/transport\]|^\[5a0391aab23c0a5\]|^\[Report Only\]|^fun-hooks|^%cAd URL:%c|^bkTags|^loaded|^\[amp-story-auto-ads:ui\]|^\[DOM\] Found 2 elements/gi;
       if (regExp.test(message.text())) return;
       const LoggerKeys: Array<LogLevel> = ['log', 'error', 'warn', 'debug', 'verbose'];
       const LoggerType = LoggerKeys.find(key => new RegExp(key, 'gi').test(message.type())) ?? 'log';
