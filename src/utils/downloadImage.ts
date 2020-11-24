e71b2cd47087617ae111dcc149a5152a75d0bfb6
ukyiJS <ukyi.js@gmail.com>
2020-11-24 20:25:16 +0900

    feat: multipleDownloads 제거

--- src/utils/downloadImage.ts
+++ src/utils/downloadImage.ts
@@ -2,20 +2,19 @@ import { Logger } from '@nestjs/common';
 import axios, { AxiosPromise } from 'axios';
 import { createWriteStream, existsSync, mkdirSync, ReadStream } from 'fs';
 import { join } from 'path';
-import { ProgressBar } from '.';
 
 export type DataToDownload = {
+  no?: string;
   url: string;
   fileName: string;
 };
 
 export class DownloadImage {
-  constructor(readonly dir = join(process.cwd(), 'download')) {}
-
-  public download = async (url: string, fileName: string): Promise<ReadStream | null> => {
-    if (!existsSync(this.dir)) {
-      mkdirSync(this.dir);
-      Logger.log(this.dir, 'CreateDirectory');
+  public download = async (url: string, fileName: string, dirName = 'download'): Promise<void> => {
+    const dir = join(process.cwd(), dirName);
+    if (!existsSync(dir)) {
+      mkdirSync(dir);
+      Logger.log(dir, 'CreateDirectory');
     }
 
     let download = <ReadStream>{};
@@ -24,32 +23,14 @@ export class DownloadImage {
       download = data;
     } catch (error) {
       Logger.error(error.message, error.stack, 'Download Error');
-      return null;
+      return undefined;
     }
 
     return new Promise(resolve => {
       download
-        .pipe(createWriteStream(`${this.dir}/${fileName}`))
+        .pipe(createWriteStream(`${dir}/${fileName}`))
         .on('finish', resolve)
         .on('error', error => Logger.error(error.message, error.stack, error.name));
     });
-  };
-
-  public multipleDownloads = async (dataToDownloads: DataToDownload[]): Promise<ReadStream[]> => {
-    let result = <ReadStream[]>[];
-    const loading = new ProgressBar();
-    const loadingSize = dataToDownloads.length;
-
-    for (const [index, { url, fileName }] of dataToDownloads.entries()) {
-      const download = await this.download(url, fileName);
-      const cursor = index + 1;
-      Logger.log(`${cursor} : ${fileName}`, 'Download');
-      loading.update((cursor / loadingSize) * 100);
-
-      if (!download) continue;
-      result = [...result, download];
-    }
-
-    return result;
   };
 }
