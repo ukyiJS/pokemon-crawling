cdaacc81042decc465600c5407665fe2536ea84d
ukyiJS <ukyi.js@gmail.com>
2020-11-27 00:02:09 +0900

    feat: PokemonOfWiki 스키마 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { PokemonOfWiki } from './model/pokemonOfWiki.entity';
 import { PokemonController } from './pokemon.controller';
 import { PokemonResolver } from './pokemon.resolver';
 import { PokemonService } from './pokemon.service';
 
 @Module({
   providers: [PokemonService, PokemonResolver],
-  imports: [TypeOrmModule.forFeature([PokemonOfDatabase])],
+  imports: [TypeOrmModule.forFeature([PokemonOfDatabase, PokemonOfWiki])],
   controllers: [PokemonController],
   exports: [PokemonService],
 })
