bf9be041002599fa8b9de2d7399b04765f74ea04
UkYiJS <ukyi.js@gmail.com>
2020-11-30 03:57:23 +0900

    feat: ProgressBar 추가

--- src/utils/progressBar.ts
+++ src/utils/progressBar.ts
@@ -1,4 +1,3 @@
-import { Logger } from '@nestjs/common';
 import { bgGreenBright, cyanBright, greenBright, redBright, whiteBright, yellowBright } from 'chalk';
 import { stdout } from 'process';
 
@@ -9,43 +8,34 @@ export type ProgressType = typeof STDOUT | typeof LOG;
 export class ProgressBar {
   private percent = 100;
   private cursor = 0;
-  private type: ProgressType;
   private color = whiteBright;
+  private progressBarSize: number;
 
-  constructor(type: ProgressType = STDOUT) {
-    this.type = type;
+  constructor(progressBarSize: number) {
+    this.progressBarSize = progressBarSize;
   }
 
-  private printProgress = (context = ''): void => {
+  private printProgressBar = (context = ''): void => {
     const dots = '\u2592'.repeat(this.cursor / 2);
     const left = this.percent / 2 - Math.floor(this.cursor / 2);
     const empty = '\u2591'.repeat(left);
     const progressBar = this.color(`\r[${dots}${empty}] ${this.cursor}%`);
     const label = context ? bgGreenBright.black(` ${context} `) : '';
-
     const message = `${progressBar} ${label}\r`;
 
     if (this.cursor === 100) {
       stdout.write(`${progressBar} ${bgGreenBright.black('Completion')}\r\n`);
       return;
     }
-    switch (this.type) {
-      case LOG:
-        Logger.log(message, 'Progress');
-        break;
-      default:
-        stdout.write(message);
-        break;
-    }
+    stdout.write(message);
   };
-
-  public update = (cursor: number, message?: string): void => {
-    this.cursor = Math.floor(cursor);
+  public updateProgressBar = (cursor: number, context?: string): void => {
+    this.cursor = Math.floor(cursor / this.progressBarSize);
     if (this.cursor < 33) this.color = redBright;
     else if (this.cursor < 66) this.color = yellowBright;
     else if (this.cursor < 100) this.color = cyanBright;
     else this.color = greenBright;
 
-    this.printProgress(message);
+    this.printProgressBar(context);
   };
 }
