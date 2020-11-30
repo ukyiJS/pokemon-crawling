66f343451a89b78ee94d622cd2cce87735adcd3c
UkYiJS <ukyi.js@gmail.com>
2020-12-01 00:46:18 +0900

    feat: pokemon resolver 추가

--- src/pokemon/pokemon.resolver.ts
+++ src/pokemon/pokemon.resolver.ts
@@ -1,19 +1,9 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { FindAndModifyWriteOpResultObject } from 'typeorm';
-import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { Mutation, Resolver } from '@nestjs/graphql';
 import { PokemonService } from './pokemon.service';
 
 @Resolver()
 export class PokemonResolver {
   constructor(private readonly pokemonService: PokemonService) {}
-
-  @Query(() => [PokemonOfDatabase])
-  public async findPokemonOfDatabase(
-    @Args('page', { type: () => Int, nullable: true }) page?: number,
-    @Args('display', { type: () => Int, nullable: true }) display?: number,
-  ): Promise<PokemonOfDatabase[]> {
-    return this.pokemonService.findPokemonOfDatabase(page, display);
-  }
 
   @Mutation(() => Boolean)
   public async addPokemonOfWiki(): Promise<boolean> {
@@ -23,25 +13,5 @@ export class PokemonResolver {
   @Mutation(() => Boolean)
   public async addPokemonOfDatabase(): Promise<boolean> {
     return this.pokemonService.addPokemonOfDatabase();
-  }
-
-  @Query(() => Boolean)
-  public async downloadPokemonImagesOfSerebiiNet(): Promise<boolean> {
-    return this.pokemonService.downloadPokemonImagesOfSerebiiNet();
-  }
-
-  @Query(() => Boolean)
-  public async downloadPokemonIconImagesOfSerebiiNet(): Promise<boolean> {
-    return this.pokemonService.downloadPokemonIconImagesOfSerebiiNet();
-  }
-
-  @Mutation(() => [PokemonOfDatabase])
-  public async updatePokemonImageOfDatabase(): Promise<FindAndModifyWriteOpResultObject[]> {
-    return this.pokemonService.updatePokemonImageOfDatabase();
-  }
-
-  @Mutation(() => [PokemonOfDatabase])
-  public async updatePokemonIconImageOfDatabase(): Promise<FindAndModifyWriteOpResultObject[]> {
-    return this.pokemonService.updatePokemonIconImageOfDatabase();
   }
 }
