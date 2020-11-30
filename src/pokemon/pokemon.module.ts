5f9c6d8020aba07c252b421e9cccc622412edc85
UkYiJS <ukyi.js@gmail.com>
2020-12-01 00:44:11 +0900

    feat: TypeOrmModule 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
-import { PokemonOfWiki } from './model/pokemonOfWiki.entity';
+import { PokemonOfDatabaseEntity } from './model/pokemonOfDatabase.entity';
+import { PokemonOfWikiEntity } from './model/pokemonOfWiki.entity';
 import { PokemonController } from './pokemon.controller';
 import { PokemonResolver } from './pokemon.resolver';
 import { PokemonService } from './pokemon.service';
 
 @Module({
   providers: [PokemonService, PokemonResolver],
-  imports: [TypeOrmModule.forFeature([PokemonOfDatabase, PokemonOfWiki])],
+  imports: [TypeOrmModule.forFeature([PokemonOfWikiEntity, PokemonOfDatabaseEntity])],
   controllers: [PokemonController],
   exports: [PokemonService],
 })
