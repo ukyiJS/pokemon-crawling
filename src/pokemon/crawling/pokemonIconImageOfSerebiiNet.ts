f096ed9915b309e8b07e8900d91152d0e27fd221
UkYiJS <ukyi.js@gmail.com>
2020-11-30 11:26:42 +0900

    feat: CrawlingPokemonIconImageOfSerebiiNet 추가

---
+++ src/pokemon/crawling/pokemonIconImageOfSerebiiNet.ts
@@ -0,0 +1,51 @@
+import { IPokemonImage } from '@/pokemon/pokemon.interface';
+import { Page } from 'puppeteer-extra/dist/puppeteer';
+
+export class CrawlingPokemonIconImageOfSerebiiNet {
+  public crawling = async (page: Page): Promise<IPokemonImage[]> => {
+    return page.evaluate((): IPokemonImage[] => {
+      const { of } = new (class {
+        private $element: Element | null;
+        private $elements: Element[];
+
+        public of = ($element?: Element | Element[] | NodeListOf<Element> | null): this => {
+          if (!$element) this.$element = null;
+          else if ($element instanceof Element) this.$element = $element;
+          else this.$elements = Array.from($element);
+
+          return this;
+        };
+        public getElement = (): Element | null => this.$element;
+        public getElements = (): Element[] => this.$elements;
+        public getText = (): string => (<Element | null>this.$element)?.textContent?.trim().replace(/é/gi, 'e') ?? '';
+        public getTexts = (): string[] => {
+          return this.$elements.reduce<string[]>((acc, $element) => {
+            const text = $element.textContent?.trim().replace(/é/gi, 'e') ?? '';
+            return text ? [...acc, text] : acc;
+          }, []);
+        };
+        public matchText = (regExp: RegExp): string => this.getText().match(regExp)?.[1] ?? '';
+        public replaceText = (searchValue: string | RegExp, replaceValue = ''): string => {
+          return this.getText().replace(new RegExp(searchValue, 'gi'), replaceValue);
+        };
+        public getColumn = (): Element[][] => {
+          return this.$elements.map(e => Array.from(e.querySelectorAll('td.fooinfo:nth-child(-n + 3)')));
+        };
+        public getSrc = (): string => this.$element?.querySelector('img')?.src ?? '';
+        public getHref = (regExp?: RegExp): string => {
+          const href = (<HTMLAnchorElement>this.$element)?.href;
+          return (regExp ? href?.match(regExp)?.[1] : href) ?? '';
+        };
+      })();
+
+      return of(document.querySelectorAll('#content > main > table > tbody > tr:nth-child(n + 3)'))
+        .getColumn()
+        .map(([$no, $image, $name]) => {
+          const no = of($no).replaceText(/\D/);
+          const image = of($image).getSrc();
+          const name = of($name).getText();
+          return { no, name, image };
+        });
+    });
+  };
+}
