fc7ffd771aa8d0ad1f9394fa33d9465259614d6d
UkYiJS <ukyi.js@gmail.com>
2020-12-01 14:44:24 +0900

    feat: EggCycleType 추가

---
+++ src/pokemon/types/eggCycle.type.ts
@@ -0,0 +1,9 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export abstract class EggCycleType {
+  @Field(() => Int)
+  public value: number;
+  @Field(() => Int, { nullable: true })
+  public step: number[] | null;
+}
