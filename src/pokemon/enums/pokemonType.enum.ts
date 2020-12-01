d020187b4d006811e9ffba8f92369bd6fbcaa57e
UkYiJS <ukyi.js@gmail.com>
2020-12-01 15:58:45 +0900

    feat: PokemonType enum 추가

---
+++ src/pokemon/enums/pokemonType.enum.ts
@@ -0,0 +1,21 @@
+export enum PokemonTypes {
+  NORMAL = '노말',
+  FIRE = '불',
+  WATER = '물',
+  ELECTRIC = '전기',
+  GRASS = '풀',
+  ICE = '얼음',
+  FIGHTING = '격투',
+  POISON = '독',
+  GROUND = '땅',
+  FLYING = '비행',
+  PSYCHIC = '에스퍼',
+  BUG = '벌레',
+  ROCK = '바위',
+  GHOST = '고스트',
+  DRAGON = '드래곤',
+  DARK = '악',
+  STEEL = '강철',
+  FAIRY = '페어리',
+}
+export type PokemonType = typeof PokemonTypes[keyof typeof PokemonTypes];
