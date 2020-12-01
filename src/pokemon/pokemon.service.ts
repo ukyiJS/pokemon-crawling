e4d89d9d041fe065b1eebab32c0bf7ea76e62668
UkYiJS <ukyi.js@gmail.com>
2020-12-01 14:40:56 +0900

    feat: pokemon service 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,28 +1,32 @@
 import { getJson, Puppeteer } from '@/utils';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
-import { CrawlingPokemonOfDatabase, CrawlingPokemonsOfWiki } from './crawling';
+import { CrawlingPokemonDatabase } from './crawling/pokemonDatabase';
 import { CrawlingPokemonIconImageOfSerebiiNet } from './crawling/pokemonIconImageOfSerebiiNet';
 import { CrawlingPokemonImageOfSerebiiNet } from './crawling/pokemonImageOfSerebiiNet';
-import { PokemonOfDatabaseEntity } from './model/pokemonOfDatabase.entity';
-import { PokemonOfWikiEntity } from './model/pokemonOfWiki.entity';
-import { IPokemonImage, PokemonOfDatabase, PokemonOfWiki } from './pokemon.interface';
+import { CrawlingPokemonsWiki } from './crawling/PokemonsWiki';
+import { PokemonNames } from './enums/pokemonName.enum';
+import { IPokemonDatabase } from './interfaces/pokemonDatabase.interface';
+import { IPokemonWiki } from './interfaces/pokemonWiki.interface';
+import { PokemonDatabase } from './model/pokemonDatabase.entity';
+import { PokemonWiki } from './model/pokemonWiki.entity';
+import { IPokemonImage } from './pokemon.interface';
 
 @Injectable()
 export class PokemonService extends Puppeteer {
   constructor(
-    @InjectRepository(PokemonOfWikiEntity)
-    private readonly pokemonOfWiKiRepository: MongoRepository<PokemonOfWikiEntity>,
-    @InjectRepository(PokemonOfDatabaseEntity)
-    private readonly pokemonOfDatabaseRepository: MongoRepository<PokemonOfDatabaseEntity>,
+    @InjectRepository(PokemonWiki)
+    private readonly pokemonWiKiRepository: MongoRepository<PokemonWiki>,
+    @InjectRepository(PokemonDatabase)
+    private readonly pokemonDatabaseRepository: MongoRepository<PokemonDatabase>,
   ) {
     super();
   }
 
-  public async getPokemonsOfWiki(): Promise<PokemonOfWiki[]> {
+  public async getPokemonWiki(): Promise<IPokemonWiki[]> {
     const { browser, page } = await this.initPuppeteer('https://pokemon.fandom.com/ko/wiki/이상해씨');
-    const { crawling } = new CrawlingPokemonsOfWiki();
+    const { crawling } = new CrawlingPokemonsWiki();
 
     const pokemons = await crawling(page);
     await browser.close();
@@ -30,9 +34,9 @@ export class PokemonService extends Puppeteer {
     return pokemons;
   }
 
-  public async getPokemonsOfDatabase(): Promise<PokemonOfDatabase[]> {
+  public async getPokemonDatabase(): Promise<IPokemonDatabase[]> {
     const { browser, page } = await this.initPuppeteer('https://pokemondb.net/pokedex/bulbasaur');
-    const { crawling } = new CrawlingPokemonOfDatabase();
+    const { crawling } = new CrawlingPokemonDatabase();
 
     const pokemons = await crawling(page);
     await browser.close();
@@ -40,7 +44,7 @@ export class PokemonService extends Puppeteer {
     return pokemons;
   }
 
-  public async getPokemonIconImageOfSerebiiNet(): Promise<IPokemonImage[]> {
+  public async getPokemonIconImagOfSerebiiNet(): Promise<IPokemonImage[]> {
     const { browser, page } = await this.initPuppeteer('https://serebii.net/pokemon/nationalpokedex.shtml');
     const { crawling } = new CrawlingPokemonIconImageOfSerebiiNet();
 
@@ -50,7 +54,7 @@ export class PokemonService extends Puppeteer {
     return pokemonIconImages;
   }
 
-  public async getPokemonImagesOfSerebiiNet(): Promise<IPokemonImage[]> {
+  public async getPokemonImageOfSerebiiNet(): Promise<IPokemonImage[]> {
     const { browser, page } = await this.initPuppeteer('https://serebii.net/pokemon/bulbasaur');
     const { crawling } = new CrawlingPokemonImageOfSerebiiNet();
 
@@ -60,25 +64,56 @@ export class PokemonService extends Puppeteer {
     return pokemonImages;
   }
 
-  public async addPokemonOfWiki(): Promise<boolean> {
-    const pokemons = getJson<PokemonOfWiki[]>({ fileName: 'pokemonsOfWiki.json' });
+  public async addPokemonWiki(): Promise<boolean> {
+    const pokemons = getJson<IPokemonWiki[]>({ fileName: 'pokemonsOfWiki.json' });
     if (!pokemons) return false;
 
-    const savePokemon = (pokemon: PokemonOfWikiEntity) => {
-      return this.pokemonOfWiKiRepository.save(new PokemonOfWikiEntity(pokemon));
+    const savePokemon = (pokemon: PokemonWiki) => {
+      return this.pokemonWiKiRepository.save(new PokemonWiki(pokemon));
     };
     await Promise.all(pokemons.map(savePokemon));
     return true;
   }
 
-  public async addPokemonOfDatabase(): Promise<boolean> {
-    const pokemons = getJson<PokemonOfDatabase[]>({ fileName: 'pokemonsOfDatabase.json' });
+  public async addPokemonDatabase(): Promise<boolean> {
+    const pokemons = getJson<IPokemonDatabase[]>({ fileName: 'pokemonsOfDatabase.json' });
     if (!pokemons) return false;
 
-    const savePokemon = (pokemon: PokemonOfDatabaseEntity) => {
-      return this.pokemonOfDatabaseRepository.save(new PokemonOfDatabaseEntity(pokemon));
+    const savePokemon = (pokemon: PokemonDatabase) => {
+      return this.pokemonDatabaseRepository.save(new PokemonDatabase(pokemon));
     };
     await Promise.all(pokemons.map(savePokemon));
     return true;
+  }
+
+  private convertToKorName = <T>(enums: T, nameToConvert: string) => {
+    const removeSpecialSymbol = (text: string) => {
+      return text.replace(/(♂)|(♀)/, (str, $1, $2) => ($1 && 'M') || ($2 && 'F') || str).replace(/[^a-z0-9]/gi, '');
+    };
+
+    let result;
+    try {
+      [, result] = Object.entries(enums).find(([key]) => {
+        return RegExp(`${removeSpecialSymbol(key)}$`, 'gi').test(removeSpecialSymbol(nameToConvert));
+      })!;
+    } catch (error) {
+      Logger.error(`No matching ${nameToConvert} found`, 'NoMatchingError');
+    }
+    return result;
+  };
+
+  public async updatePokemonName(): Promise<PokemonDatabase[]> {
+    const pokemons = getJson<IPokemonDatabase[]>({ fileName: 'pokemonsOfDatabase.json' });
+    if (!pokemons) return [];
+
+    const convertToKorName = this.convertToKorName.bind(null, PokemonNames);
+    return pokemons.map(pokemon => {
+      const name = convertToKorName(pokemon.name);
+      const differentForm = pokemon.differentForm?.map(({ name, ...differentForm }) => {
+        return { ...differentForm, name: convertToKorName(name) };
+      });
+
+      return { ...pokemon, name, differentForm };
+    });
   }
 }
