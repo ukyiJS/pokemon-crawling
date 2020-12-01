3a6e8702b69e353a0d465d647a1c9eb74acc737f
UkYiJS <ukyi.js@gmail.com>
2020-12-01 14:44:48 +0900

    feat: PokemonDatabaseType 추가

---
+++ src/pokemon/types/pokemonDatabase.type.ts
@@ -0,0 +1,19 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { IPokemon } from '../interfaces/pokemon.interface';
+import { EggCycleType } from './eggCycle.type';
+import { StatType } from './stat.type';
+import { TypeDefenseType } from './typeDefense.type';
+
+@ObjectType()
+export abstract class PokemonDatabaseType extends IPokemon {
+  @Field(() => String, { nullable: true })
+  public evYield: string | null;
+  @Field(() => Int)
+  public exp: number;
+  @Field(() => [EggCycleType], { nullable: true })
+  public eggCycle: EggCycleType | null;
+  @Field(() => [StatType])
+  public stats: StatType[];
+  @Field(() => [TypeDefenseType])
+  public typeDefenses: TypeDefenseType[];
+}
