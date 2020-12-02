f373e3fbf12b0d18328bafe67b460bd10d47fa79
UkYiJS <ukyi.js@gmail.com>
2020-12-02 13:17:16 +0900

    feat: UseItemConditionNames 추가

--- src/pokemon/enums/conditionName.enum.ts
+++ src/pokemon/enums/conditionName.enum.ts
@@ -21,6 +21,25 @@ export enum LevelAdditionalConditionNames {
 }
 export type LevelAdditionalConditionName = typeof LevelAdditionalConditionNames[keyof typeof LevelAdditionalConditionNames];
 
+export enum UseItemConditionNames {
+  THUNDER_STONE = '천둥의돌',
+  ICE_STONE = '얼음의돌',
+  MOON_STONE = '달의돌',
+  FIRE_STONE = '불꽃의돌',
+  LEAF_STONE = '리프의돌',
+  SUN_STONE = '태양의돌',
+  WATER_STONE = '물의돌',
+  GALARICA_CUFF = '가라두구팔찌',
+  GALARICA_WREATH = '가라두구머리장식',
+  SHINY_STONE = '빛의돌',
+  DUSK_STONE = '어둠의돌',
+  DAWN_STONE = '각성의돌',
+  TART_APPLE = '새콤한 사과',
+  SWEET_APPLE = '달콤한 사과',
+  CRACKED_POT = '깨진 포트 or 이빠진 포트',
+}
+export type UseItemConditionName = typeof UseItemConditionNames[keyof typeof UseItemConditionNames];
+
 export enum AdditionalConditionNames {
   IN_ALOLA = '알로라 지방에서',
   IN_GALAR = '가라르 지방에서',
