55d6044a29ce9b142b592bfa20c8d9131c46e704
UkYiJS <ukyi.js@gmail.com>
2020-12-02 13:19:53 +0900

    feat: type 제거

--- src/pokemon/type/evolutionType.ts
+++
@@ -1,8 +0,0 @@
-export const evolutionType = {
-  LEVEL: 'level',
-  STONE: 'stone',
-  TRADE: 'trade',
-  FRIENDSHIP: 'friendship',
-  STATUS: 'status',
-} as const;
-export type EvolutionType = typeof evolutionType[keyof typeof evolutionType];
--- src/pokemon/type/friendshipCondition.ts
+++
@@ -1,4 +0,0 @@
-import { levelCondition } from './levelCondition';
-
-export const friendshipCondition = { ...levelCondition } as const;
-export type FriendshipCondition = typeof friendshipCondition[keyof typeof friendshipCondition];
