308661215e085dccee47ed92f3811ce579923cf0
ukyiJS <ukyi.js@gmail.com>
2020-12-03 02:35:34 +0900

    feat: ResolveField 제거

--- src/pokemon/resolvers/pokemonDatabase.resolver.ts
+++ src/pokemon/resolvers/pokemonDatabase.resolver.ts
@@ -21,18 +21,6 @@ export class PokemonDatabaseResolver {
     return pokemons;
   }
 
-  @ResolveField(() => String)
-  public async image(@Parent() pokemon: PokemonDatabase): Promise<string> {
-    const [{ image }] = (await this.pokemonService.updateImageOfPokemonDatabase([pokemon]))!;
-    return image;
-  }
-
-  @ResolveField(() => String)
-  public async icon(@Parent() pokemon: PokemonDatabase): Promise<string> {
-    const [{ icon }] = (await this.pokemonService.updateIconImageOfPokemonDatabase([pokemon]))!;
-    return <string>icon;
-  }
-
   @Mutation(() => [PokemonDatabase], { nullable: true })
   public async updateImageOfPokemonDatabase(): Promise<PokemonDatabase[] | null> {
     return this.pokemonService.updateImageOfPokemonDatabase(this.pokemons);
