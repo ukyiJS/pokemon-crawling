40d19d8736c28b9637ec926a7acf4e4486198bbb
UkYiJS <ukyi.js@gmail.com>
2020-12-02 13:16:23 +0900

    feat: AdditionalConditionNames 추가

--- src/pokemon/enums/conditionName.enum.ts
+++ src/pokemon/enums/conditionName.enum.ts
@@ -20,3 +20,24 @@ export enum LevelAdditionalConditionNames {
   WITH_AN_AMPEDNATURE = '외로움, 대담, 무사태평, 조심, 의젓, 수줍음, 냉정, 차분, 얌전, 신중, 겁쟁이, 성실 성격일 때',
 }
 export type LevelAdditionalConditionName = typeof LevelAdditionalConditionNames[keyof typeof LevelAdditionalConditionNames];
+
+export enum AdditionalConditionNames {
+  IN_ALOLA = '알로라 지방에서',
+  IN_GALAR = '가라르 지방에서',
+  IN_GRASS = '풀숲에서',
+  IN_CAVES = '동굴•사막•해안가에서',
+  IN_BUILDINGS = '건물에서',
+  POKEBALL_IN_BAG = '몬스터볼을 가지고 있는 상태에서',
+  IN_POKEMON_SUN_OR_ULTRASUN = '썬, 울트라썬, 소드 버전에서',
+  IN_POKEMON_MOON_OR_ULTRAMOON = '문, 울트라문, 실드 버전에서',
+  MALE = '수컷일 때',
+  FEMALE = '암컷일 때',
+  OR_LEVEL_UP_IN_A_MAGNETIC_FIELD_AREA = '천관산(DPPt), 전기돌동굴(BW/BW2), 13번 도로-발전소가 있는 곳(XY), 뉴보라(ORAS), 포니대협곡(SM/USUM), 화끈산(USUM)에서 레벨업 또는',
+  IN_A_MAGNETIC_FIELD_AREA = '천관산(DPPt), 전기돌동굴(BW/BW2), 13번 도로-발전소가 있는 곳(XY), 뉴보라(ORAS), 포니대협곡(SM/USUM), 화끈산(USUM)에서',
+  OR_LEVEL_UP_WITH_MAX_BEAUTY = '아름다움 컨디션이 170 이상일 때 레벨업 또는',
+  NIGHTTIME = '밤에',
+  DAYTIME = '낮에',
+}
+export type AdditionalConditionName = typeof AdditionalConditionNames[keyof typeof AdditionalConditionNames];
+
+export type Conditions = [string, string | undefined, string | undefined];
