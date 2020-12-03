171d014cbbcf0fa28850328036cc95e69e1a0962
ukyiJS <ukyi.js@gmail.com>
2020-12-03 14:28:07 +0900

    feat: getUniqueObjectArray 추가

--- src/utils/crawling/index.ts
+++ src/utils/crawling/index.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { Page } from 'puppeteer-extra/dist/puppeteer';
+import { ObjectLiteral } from 'typeorm';
 
 export class CrawlingUtil {
   public onPageClick = async (page: Page, selector: string): Promise<void> => {
@@ -10,5 +11,9 @@ export class CrawlingUtil {
       if (error.name !== 'TimeoutError') throw error;
       Logger.error(error.message, error.stack, error.name);
     }
+  };
+
+  public getUniqueObjectArray = <T>(objectArray: T[], key: string): T[] => {
+    return [...new Map(objectArray.map(item => [(<ObjectLiteral>item)[key], item])).values()];
   };
 }
