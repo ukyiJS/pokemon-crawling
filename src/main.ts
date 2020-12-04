811617ce55570c2a8f8338d9d5e1d1905d1bdeea
ukyiJS <ukyi.js@gmail.com>
2020-12-04 17:11:38 +0900

    feat: port config 추가

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { blueBright } from 'chalk';
 import { renderFile } from 'ejs';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { PORT } from './env';
-import { validateEnv } from './utils';
 
 const createServer = async () => {
-  validateEnv();
   const app = await NestFactory.create<INestApplication & NestExpressApplication>(AppModule, { cors: true });
+  const config = app.get(ConfigService);
+  const port = config.get('port');
 
   app.engine('html', renderFile).set('view engine', 'html');
   await app
     .setBaseViewsDir(join(process.cwd(), 'src/views'))
     .useStaticAssets(join(process.cwd(), 'src/assets'))
     .useStaticAssets(join(process.cwd(), 'node_modules/axios/dist'))
-    .listen(PORT);
+    .listen(port);
 
-  Logger.log(`🚀 Server running on ${blueBright(`http://localhost:${PORT}`)}`, 'Server', false);
+  Logger.log(`🚀 Server running on ${blueBright(`http://localhost:${port}`)}`, 'Server', false);
 };
 createServer();
