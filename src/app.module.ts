e8a52b98ac8a5c05f24e4ea8b656d5dbb023ba6b
ukyiJS <ukyi.js@gmail.com>
2020-12-04 17:11:23 +0900

    feat: ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { GraphqlService, TypeormService } from '@/config';
 import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,8 +8,10 @@ import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { LoggingInterceptor, TimeoutInterceptor } from './common';
 import { HttpExceptionFilter } from './common/filters/httpExceptionFilter';
+import { envConfig } from './config';
 import { DateScalar } from './config/graphql/scalars/date.scalar';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { validationSchema } from './utils';
 
 @Module({
   providers: [
@@ -20,6 +23,13 @@ import { PokemonModule } from './pokemon/pokemon.module';
     { provide: APP_INTERCEPTOR, useClass: TimeoutInterceptor },
   ],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod',
+      load: [envConfig],
+      validationSchema,
+      validationOptions: { abortEarly: true },
+    }),
     GraphQLModule.forRootAsync({ useClass: GraphqlService }),
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
     PokemonModule,
