2779179e338d4c32944ad01625909653177b932f
ukyiJS <ukyi.js@gmail.com>
2020-12-04 18:50:59 +0900

    feat: CrawlingModule 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,6 +1,7 @@
 import { ConvertModule, PuppeteerModule } from '@/utils';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CrawlingModule } from './crawling/crawling.module';
 import { PokemonDatabase } from './model/pokemonDatabase.entity';
 import { PokemonWiki } from './model/pokemonWiki.entity';
 import { PokemonController } from './pokemon.controller';
@@ -9,7 +10,7 @@ import * as resolvers from './resolvers';
 
 @Module({
   providers: [PokemonService, ...Object.values(resolvers)],
-  imports: [TypeOrmModule.forFeature([PokemonWiki, PokemonDatabase]), PuppeteerModule, ConvertModule],
+  imports: [TypeOrmModule.forFeature([PokemonWiki, PokemonDatabase]), PuppeteerModule, ConvertModule, CrawlingModule],
   controllers: [PokemonController],
 })
 export class PokemonModule {}
