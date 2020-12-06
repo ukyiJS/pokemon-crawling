6802a172b6cf2a3090289fc69febd4d488bb31e6
UkYiJS <ukyi.js@gmail.com>
2020-12-06 21:53:44 +0900

    test: createTestingModule 추가

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,11 +1,12 @@
-import { envConfig, TypeormService } from '@/config';
-import { ConvertModule, PuppeteerModule, validationSchema } from '@/utils';
+import { configOptions, TypeormService } from '@/config';
+import { ConvertModule, getJson, PuppeteerModule } from '@/utils';
 import { ConfigModule } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrawlingModule } from './crawling/crawling.module';
 import { PokemonDatabase } from './model/pokemonDatabase.entity';
 import { PokemonWiki } from './model/pokemonWiki.entity';
+import { SerebiiNet } from './model/serebiiNet.entity';
 import { PokemonService } from './pokemon.service';
 
 jest.setTimeout(100000);
@@ -15,13 +16,7 @@ describe('PokemonService', () => {
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod',
-          load: [envConfig],
-          validationSchema,
-          validationOptions: { abortEarly: true },
-        }),
+        ConfigModule.forRoot(configOptions),
         TypeOrmModule.forRootAsync({ useClass: TypeormService }),
         TypeOrmModule.forFeature([PokemonWiki, PokemonDatabase]),
         PuppeteerModule,
@@ -32,24 +27,5 @@ describe('PokemonService', () => {
     }).compile();
 
     service = await moduleRef.resolve(PokemonService);
-  });
-
-  it.only('Pokemons of Wiki Test', async () => {
-    const pokemons = await service.crawlingPokemonWiki();
-    expect(pokemons).not.toHaveLength(0);
-  });
-
-  it('Pokemons of Database Test', async () => {
-    const pokemons = await service.crawlingPokemonDatabase();
-    expect(pokemons).not.toHaveLength(0);
-  });
-
-  it('Pokemon Images Test', async () => {
-    const pokemons = await service.crawlingPokemonImageOfSerebiiNet();
-    expect(pokemons).not.toHaveLength(0);
-  });
-  it('Pokemon Icon Images Test', async () => {
-    const pokemons = await service.crawlingPokemonIconImagOfSerebiiNet();
-    expect(pokemons).not.toHaveLength(0);
   });
 });
