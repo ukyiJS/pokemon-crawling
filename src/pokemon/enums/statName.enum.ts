5c445311744bfd3802d2d80e2d12f54715e4d8d4
UkYiJS <ukyi.js@gmail.com>
2020-12-06 23:50:24 +0900

    feat: StatName enum 추가

---
+++ src/pokemon/enums/statName.enum.ts
@@ -0,0 +1,8 @@
+export enum StatNames {
+  ATTACK = '공격',
+  DEFENSE = '방어',
+  SPECIAL_ATTACK = '특수공격',
+  SPECIAL_DEFENSE = '특수방어',
+  SPEED = '스피드',
+}
+export type StatName = typeof StatNames[keyof typeof StatNames];
