04407c04951eed0f6c6daef7a3ae82726216ff8a
UkYiJS <ukyi.js@gmail.com>
2020-12-07 00:21:40 +0900

    feat: IPokemonDatabase 추가

--- src/pokemon/interfaces/pokemonDatabase.interface.ts
+++ src/pokemon/interfaces/pokemonDatabase.interface.ts
@@ -1,11 +1,24 @@
-import { Field, InterfaceType } from '@nestjs/graphql';
+import { Field, Int, InterfaceType } from '@nestjs/graphql';
+import { EggCycleType } from '../types/eggCycle.type';
 import { EvolvingToType } from '../types/evolvingTo.type';
 import { PokemonDatabaseType } from '../types/pokemonDatabase.type';
+import { StatType } from '../types/stat.type';
+import { TypeDefenseType } from '../types/typeDefense.type';
 
 @InterfaceType()
 export abstract class IPokemonDatabase extends PokemonDatabaseType {
   @Field({ nullable: true })
   public icon?: string;
+  @Field(() => [String], { nullable: true })
+  public evYield: string[] | null;
+  @Field(() => Int)
+  public exp: number;
+  @Field(() => EggCycleType, { nullable: true })
+  public eggCycle: EggCycleType | null;
+  @Field(() => [StatType])
+  public stats: StatType[];
+  @Field(() => [TypeDefenseType])
+  public typeDefenses: TypeDefenseType[];
   @Field(() => [EvolvingToType], { nullable: true })
   public evolvingTo?: EvolvingToType[];
   @Field(() => [PokemonDatabaseType], { nullable: true })
